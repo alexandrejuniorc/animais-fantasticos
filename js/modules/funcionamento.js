c33e89106d52efab7c3339e2f6eeed9ba01d45f8
Alexandre Junior <alexandrejunior408@gmail.com>
2022-02-22 13:58:50 -0300

    corrige posição da função de funcionamento

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,22 +1,22 @@
-export default function initFuncionamento() {}
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number); // split transforma uma string em uma array
+  // adicionando o map que irá integrar cada item da array e passando Number, tornará cada item da Array em um número
 
-const funcionamento = document.querySelector("[data-semana]");
-const diasSemana = funcionamento.dataset.semana.split(",").map(Number); // split transforma uma string em uma array
-// adicionando o map que irá integrar cada item da array e passando Number, tornará cada item da Array em um número
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
 
-const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+  const dataAgora = new Date(); // data de agora
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
 
-const dataAgora = new Date(); // data de agora
-const diaAgora = dataAgora.getDay();
-const horarioAgora = dataAgora.getHours();
+  //verifica se o dia da semana é igual ao dia de hoje, se for de seg a sex funcionará, se for -1 que são os fins de semanas não irá funcionar
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
 
-//verifica se o dia da semana é igual ao dia de hoje, se for de seg a sex funcionará, se for -1 que são os fins de semanas não irá funcionar
-const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const horarioAberto =
+    horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
 
-const horarioAberto =
-  horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
-
-//se semanaAberto for true e horarioAberto true ele irá adicionar a classe aberto
-if (semanaAberto && horarioAberto) {
-  funcionamento.classList.add("aberto");
+  //se semanaAberto for true e horarioAberto true ele irá adicionar a classe aberto
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto");
+  }
 }
